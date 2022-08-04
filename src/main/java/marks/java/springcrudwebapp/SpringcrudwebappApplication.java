944b9ca921f9a3e5ef02f533336254ee0cbb4d67
Marks <andremarks867@gmail.com>
2022-08-03 23:07:10 -0500

    inicializa repositorio git para crud web app

---
+++ src/main/java/marks/java/springcrudwebapp/SpringcrudwebappApplication.java
@@ -0,0 +1,13 @@
+package marks.java.springcrudwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcrudwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcrudwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marks/java/springcrudwebapp/SpringcrudwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package marks.java.springcrudwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcrudwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
