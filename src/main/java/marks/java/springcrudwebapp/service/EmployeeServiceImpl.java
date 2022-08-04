38d2a7ff5d50a18beb86e26c903cdb76efd93774
Marks <andremarks867@gmail.com>
2022-08-04 00:27:24 -0500

    pequenas correcoes

--- src/main/java/marks/java/springcrudwebapp/service/EmployeeServiceImpl.java
+++ src/main/java/marks/java/springcrudwebapp/service/EmployeeServiceImpl.java
@@ -3,11 +3,12 @@ package marks.java.springcrudwebapp.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import marks.java.springcrudwebapp.model.Employee;
 import marks.java.springcrudwebapp.repository.EmployeeRepository;
 
-
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
   @Autowired
