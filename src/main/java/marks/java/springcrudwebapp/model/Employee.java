12d06d19615f7b9b6778f82b693a434ec733951f
Marks <andremarks867@gmail.com>
2022-08-03 23:30:43 -0500

    configura o model

---
+++ src/main/java/marks/java/springcrudwebapp/model/Employee.java
@@ -0,0 +1,41 @@
+package marks.java.springcrudwebapp.model;
+
+public class Employee {
+  private long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
